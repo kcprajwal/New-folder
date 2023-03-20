b0408937451f2867e099b269fd82f05d1bd49a3b
kcprajwal <pkc44072@gmail.com>
2023-03-20 16:49:54 +0545

    qwerty

---
+++ loop.py
@@ -0,0 +1,17 @@
+a = "baby, child!"
+print(len(a))
+
+txt = "everyone is beautiful in thier own eyes!"
+print("shristi " in txt)
+
+txt = "the best thing in life are expensive!"
+if "life" in txt:
+    print("yes,'life is present.")
+
+txt = "the best things in life is expensive!"
+print("free" not in txt)
+
+txt = "the best in life are free!"
+if "expensive" not in txt:
+    print("no, 'expensive is not present.")
+    
---
+++ pythoncasting.py
@@ -0,0 +1,24 @@
+a = int(1)
+b = int(3.6)
+c = int("8")
+
+print(a)
+print(b)
+print(c)
+
+x = float(2)
+y = float(3.6)
+z = float("5")
+q = float("7.7")
+
+print(x)
+print(y)
+print(z)
+print(q)
+
+p = str("q1")
+o = str('5')
+i = str("prajwal")
+print(p)
+print(o)
+print(i)
